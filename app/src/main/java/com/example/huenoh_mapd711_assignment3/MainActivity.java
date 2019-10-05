8ad5153150edf25653343185d1a585f372b8beb9
HUEN_OH <heons921@gmail.com>
2019-10-04 20:20:41 -0400

    add : project

---
+++ app/src/main/java/com/example/huenoh_mapd711_assignment3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.huenoh_mapd711_assignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
