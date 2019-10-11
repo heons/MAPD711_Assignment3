d0ad22f93ccc81ac6df59e9e6d09a908f72c1214
HUEN_OH <heons921@gmail.com>
2019-10-10 20:29:55 -0400

    add : Product class

---
+++ app/src/main/java/com/example/huenoh_mapd711_assignment3/Product.java
@@ -0,0 +1,64 @@
+package com.example.huenoh_mapd711_assignment3;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private double price;
+    private int quantity;
+    private String category;
+
+
+    public Product() {
+    }
+
+    public Product(int productId, String productName, double price, int quantity, String category) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+    // Getters
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+
+    // Setters
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
