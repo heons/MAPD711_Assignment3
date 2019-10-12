e0889509a098bbe86d290fcf22d76d43c310b4d6
HUEN_OH <heons921@gmail.com>
2019-10-11 20:23:10 -0400

    update : by instantiate Managers for DB in MainActivity

--- app/src/main/java/com/example/huenoh_mapd711_assignment3/MainActivity.java
+++ app/src/main/java/com/example/huenoh_mapd711_assignment3/MainActivity.java
@@ -3,12 +3,61 @@ package com.example.huenoh_mapd711_assignment3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Managers for DB
+    private CustomerManager customerManager;
+    private AdminManager adminManager;
+    private ProductManager productManager;
+    private OrderManager orderManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // instantiate the CustomerManager
+        try {
+            customerManager = new CustomerManager(this);
+        }
+        catch(Exception exception)
+        {
+            Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.i("Error: ", exception.getMessage());
+        }
+
+        // instantiate the AdminManager
+        try {
+            adminManager = new AdminManager(this);
+        }
+        catch(Exception exception)
+        {
+            Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.i("Error: ", exception.getMessage());
+        }
+
+        // instantiate the ProductManager
+        try {
+            productManager = new ProductManager(this);
+        }
+        catch(Exception exception)
+        {
+            Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.i("Error: ", exception.getMessage());
+        }
+
+        // instantiate the OrderManager
+        try {
+            orderManager = new OrderManager(this);
+        }
+        catch(Exception exception)
+        {
+            Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.i("Error: ", exception.getMessage());
+        }
     }
 }
