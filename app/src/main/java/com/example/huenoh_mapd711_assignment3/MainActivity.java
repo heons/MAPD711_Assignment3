972677027d323d0f90b064d1da92c3bd36f260c7
HUEN_OH <heons921@gmail.com>
2019-10-17 22:59:24 -0400

    add : test functions for Customer;add, retieve

--- app/src/main/java/com/example/huenoh_mapd711_assignment3/MainActivity.java
+++ app/src/main/java/com/example/huenoh_mapd711_assignment3/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.huenoh_mapd711_assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -59,5 +61,50 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
             Log.i("Error: ", exception.getMessage());
         }
+
+        // Test Customer Field in DB
+        testAddCustomer();
+        testRetrieveCustomer();
     }
+
+
+    // Test function to Add Customer
+    public void testAddCustomer() {
+        // Initialize ContentValues object with the new car
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("customerId", 1);
+        contentValues.put("userName", "heon");
+        contentValues.put("password", "pwd");
+        contentValues.put("firstName", "huen");
+        contentValues.put("lastName", "oh");
+        contentValues.put("address", "28 uptown");
+        contentValues.put("city", "markham");
+        contentValues.put("postalCode", "L3R5M8");
+
+        // Add to the DB
+        try {
+            customerManager.addRow(contentValues, CustomerManager.TABLE_NAME);
+            Toast.makeText(MainActivity.this, "a customer added", Toast.LENGTH_SHORT).show();
+        } catch (Exception exception) {
+            Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.i("Error: ", exception.getMessage());
+        }
+    }
+
+    // Test function to Retrieve Customer
+    public void testRetrieveCustomer()
+    {
+        try {
+            Customer customer = customerManager.getCustomerById("1" , "customerId");
+            Log.d("main", customer.getUserName());
+        }
+        catch (Exception exception)
+        {
+            Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.i("Error: ",exception.getMessage());
+
+        }
+    }
+
+
 }
